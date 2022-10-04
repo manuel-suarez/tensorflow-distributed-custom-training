37d74dc7cc565b87e43cbe35efea4fa4e34239d0
Arturo Suárez <manuel.suarez@cimat.mx>
2022-10-04 13:44:55 -0500

    Data

--- main.py
+++ main.py
@@ -6,3 +6,18 @@ import numpy as np
 import os
 
 print(tf.__version__)
+
+fashion_mnist = tf.keras.datasets.fashion_mnist
+
+(train_images, train_labels), (test_images, test_labels) = fashion_mnist.load_data()
+
+# Add a dimension to the array -> new shape == (28, 28, 1)
+# This is done because the first layer in our model is a convolutional
+# layer and it requires a 4D input (batch_size, height, width, channels).
+# batch_size dimension will be added later on.
+train_images = train_images[..., None]
+test_images = test_images[..., None]
+
+# Scale the images to the [0, 1] range.
+train_images = train_images / np.float32(255)
+test_images = test_images / np.float32(255)
